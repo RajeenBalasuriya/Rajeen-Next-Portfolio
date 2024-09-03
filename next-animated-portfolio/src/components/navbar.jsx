ad7b469946aed3fb0b03b27c539b0cd7c7019de8
Rajeen Balasuriya <102136700+RajeenBalasuriya@users.noreply.github.com>
2024-09-03 22:13:05 +0530

    update nav bar

--- next-animated-portfolio/src/components/navbar.jsx
+++ next-animated-portfolio/src/components/navbar.jsx
@@ -14,7 +14,7 @@ const links=[
 const NavBar=()=>{
     const [menuOpen,setMenuOpen]=useState(false);
     return(
-        <div className='h-full flex items-center justify-between text-white px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
+        <div className='h-full flex items-center justify-between text-white px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 '>
              <div className='hidden md:flex gap-4 text-white'>
              {links.map(link=>(
                             <Link href={link.url} key={link.title}>{link.title}</Link>
@@ -23,14 +23,14 @@ const NavBar=()=>{
 
 
             {/* LOGO */}
-            <div className='md:hidden'>
+            <div className='md:hidden lg:flex w-1/3 justify-center'>
                 <Link href="/" className="txt-sm bg-grey-to-purple rounded-md p-1 font-semibold flex items-center justify-center">
                   <span className='text-white font-thin mr-2'>Rajeen</span>
                   <span className='w-12 h- rounded bg-white text-black flex  items-center justify-center'>.DEV</span>
                 </Link>
             </div>
 
-            <div className="flex flex-row items-center justify-between gap-4">
+            <div className="hidden md:flex gap-4 w-1/3 justify-center">
                 <Link href='#'>
                     <Image src="/github-mark-white.svg" alt="logo" width={24} height={24}/>
 
